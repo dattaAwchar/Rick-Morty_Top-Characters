884b316c8514c1ec96274d0cb8ceb1dc00901de6
dattaAwchar <dattaawchar13@gmail.com>
2024-09-06 19:45:12 +0530

    removed all the comments and centered the items

--- src/components/RickMorty.js
+++ src/components/RickMorty.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import RickMortyItems from './RickMortyItems';
-import LoadingBar from 'react-top-loading-bar'; 
+import LoadingBar from 'react-top-loading-bar';
 
 export default class RickMorty extends Component {
   constructor() {
@@ -11,14 +11,14 @@ export default class RickMorty extends Component {
       loading: false,
       currentPage: 1,
       itemsPerPage: 15,
-      progress: 0 
+      progress: 0
     };
-    this.loadingBar = React.createRef(); 
+    this.loadingBar = React.createRef();
   }
 
   async componentDidMount() {
     this.setState({ loading: true });
-    this.loadingBar.current.continuousStart(); 
+    this.loadingBar.current.continuousStart();
     try {
       // Fetch all pages
       let allResults = [];
@@ -39,7 +39,7 @@ export default class RickMorty extends Component {
     } catch (error) {
       console.error('Error fetching data:', error);
       this.setState({ loading: false });
-      this.loadingBar.current.complete(); 
+      this.loadingBar.current.complete();
     }
   }
 
@@ -52,27 +52,27 @@ export default class RickMorty extends Component {
 
   handlePrevClick = () => {
     if (this.state.currentPage > 1) {
-      this.loadingBar.current.continuousStart(); 
+      this.loadingBar.current.continuousStart();
       const newPage = this.state.currentPage - 1;
       this.setState({
         displayedResults: this.getItemsForPage(newPage, this.state.results),
         currentPage: newPage
       });
-      window.scrollTo(0, 0); 
-      this.loadingBar.current.complete(); 
+      window.scrollTo(0, 0);
+      this.loadingBar.current.complete();
     }
   };
 
   handleNextClick = () => {
     if (this.state.currentPage < this.state.totalPages) {
-      this.loadingBar.current.continuousStart(); 
+      this.loadingBar.current.continuousStart();
       const newPage = this.state.currentPage + 1;
       this.setState({
         displayedResults: this.getItemsForPage(newPage, this.state.results),
         currentPage: newPage
       });
       window.scrollTo(0, 0);
-      this.loadingBar.current.complete(); 
+      this.loadingBar.current.complete();
     }
   };
 
@@ -81,45 +81,53 @@ export default class RickMorty extends Component {
 
     return (
       <>
-        <LoadingBar color='yellow' ref={this.loadingBar} /> 
-        <div className="container my-5 mt-5 pt-5">
+        <LoadingBar color='yellow' ref={this.loadingBar} />
+        <div className="container my-5 mt-5 pt-5" style={{
+          display: 'grid',
+          justifyContent: 'center'
+        }}>
           {loading && <p className="text-center">Loading...</p>}
-          <div className='row'>
+          <div className='row' style={{
+            display: 'flex'
+          }}>
             {displayedResults.map((element) => (
-              <div className="col-md-4 my-2" key={element.id}>
-                <RickMortyItems 
-                  imageURL={element.image} 
-                  name={element.name} 
-                  originName={element.origin.name} 
-                  originUrl={element.origin.url} 
-                  species={element.species} 
-                  type={element.type ? element.type : "Unknown Type"} 
-                  gender={element.gender} 
-                  location={element.location.url} 
-                  locationName={element.location.name} 
-                  created={element.created.slice(0, 10)} 
-                />
-              </div>
+              <div className="col-md-4 my-2" style={{
+                display: 'flex',
+                justifyContent: 'center'
+              }} key={element.id}>
+            <RickMortyItems
+              imageURL={element.image}
+              name={element.name}
+              originName={element.origin.name}
+              originUrl={element.origin.url}
+              species={element.species}
+              type={element.type ? element.type : "Unknown Type"}
+              gender={element.gender}
+              location={element.location.url}
+              locationName={element.location.name}
+              created={element.created.slice(0, 10)}
+            />
+          </div>
             ))}
-          </div>
-          <div className="container d-flex justify-content-evenly mt-4">
-            <button 
-              disabled={currentPage <= 1} 
-              type="button" 
-              onClick={this.handlePrevClick} 
-              className="btn btn-primary">
-              Previous
-            </button>
-            <span className="align-self-center">Page {currentPage} of {totalPages}</span>
-            <button 
-              disabled={currentPage >= totalPages} 
-              type="button" 
-              onClick={this.handleNextClick} 
-              className="btn btn-primary">
-              Next
-            </button>
-          </div>
         </div>
+        <div className="container d-flex justify-content-evenly mt-4">
+          <button
+            disabled={currentPage <= 1}
+            type="button"
+            onClick={this.handlePrevClick}
+            className="btn btn-primary">
+            Previous
+          </button>
+          <span className="align-self-center">Page {currentPage} of {totalPages}</span>
+          <button
+            disabled={currentPage >= totalPages}
+            type="button"
+            onClick={this.handleNextClick}
+            className="btn btn-primary">
+            Next
+          </button>
+        </div>
+      </div >
       </>
     );
   }
--- src/components/RickMortyItems.js
+++ src/components/RickMortyItems.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react'
 
 export default class RickMortyItems extends Component {
-
+    
     render() {
+        
+        let { imageURL, originName, name, species, type, gender, location, locationName, created, originUrl } = this.props;
 
-        let { imageURL,originName, name, species, type, gender, location, locationName, created, originUrl} = this.props;
-
+        
         return (
+            
             <>
-                <div className="card" style={{ width: "18rem" }}>
+                <div className="card">
                     <img src={imageURL} className="card-img-top" alt="Rick and Morty" />
                     <div className="card-body">
                         <h5 className="list-group-item">{name}</h5>
@@ -19,9 +21,9 @@ export default class RickMortyItems extends Component {
                         <li className="list-group-item">{gender}</li>
                     </ul>
                     <div className="card-body">
-                        <small className="text-body-secondary">Origin:</small> <a href={originUrl} className="card-link link-danger text-decoration-none">{originName}</a><br/>
+                        <small className="text-body-secondary">Origin:</small> <a href={originUrl} className="card-link link-danger text-decoration-none">{originName}</a><br />
 
-                        <small className="text-body-secondary">Last Seen:</small> <a href={location} className="card-link link-danger text-decoration-none">{locationName}</a><br/>
+                        <small className="text-body-secondary">Last Seen:</small> <a href={location} className="card-link link-danger text-decoration-none">{locationName}</a><br />
 
                         <small className="text-body-secondary">Created: </small><small className='text-body-dark'>{created}</small>
                     </div>
